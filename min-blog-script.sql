efcb47c9c973aa140f9b7901884ed973608c2caa
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-02-27 14:51:05 -0800

    Added SQL scripting

---
+++ min-blog-script.sql
@@ -0,0 +1,17 @@
+-- CREATE DATABASE blog
+
+USE blog;
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+	id INT auto_increment,
+    author VARCHAR(255),
+    title VARCHAR(255),
+    content VARCHAR(255),
+    timestamp DATETIME DEFAULT NOW(),
+    
+    PRIMARY KEY (id)
+);
+
+-- SELECT * FROM posts;
